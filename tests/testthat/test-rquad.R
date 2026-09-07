0256b5d4fc38dda1344b3b10b80c6eaeebc60f8c
Ayaka Ouyang <ayakaouyang@Ayakas-Laptop.local>
2026-09-07 15:22:12 -0500

    adding another test that

--- tests/testthat/test-rquad.R
+++ tests/testthat/test-rquad.R
@@ -10,7 +10,11 @@ test_that("rquad output the number within support", {
 })
 
 
-
+test_that("rquad handle invalid parameter values", {
+  expect_error(rquad(100,0))
+  expect_error(rquad("100",0))
+  expect_error(rquad(-1,1))
+})
 
 
 test_that("rquad reproducibility under set.seed", {
@@ -22,4 +26,21 @@ test_that("rquad reproducibility under set.seed", {
   x2 <- rquad(100,1)
 
   expect_equal(x1, x2)
+})
+
+
+
+
+test_that("rquad agreement between emprical and theoritical", {
+
+  theta <- 1
+
+  mean_theory <- 0
+  variance_theory <- (3*theta+5)/(5*(theta+3))
+
+  set.seed(123)
+  x <- rquad(10000,theta)
+
+  expect_equal(mean(x), mean_theory, tolerance = 0.05)
+  expect_equal(var(x), variance_theory, tolerance = 0.05)
 })
