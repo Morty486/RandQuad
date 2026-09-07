395d91755140fc0b550a8d5262dee8ffaad1875d
Ayaka Ouyang <ayakaouyang@Ayakas-Laptop.local>
2026-09-07 15:12:27 -0500

    adding test that

--- tests/testthat/test-pquad.R
+++ tests/testthat/test-pquad.R
@@ -2,3 +2,7 @@ test_that("pquad output the desired length", {
   p <- pquad(0.1,1)
   expect_length(p, 1)
 })
+
+
+
+
--- tests/testthat/test-rquad.R
+++ tests/testthat/test-rquad.R
@@ -2,3 +2,24 @@ test_that("rquad output the desired length", {
   x <- rquad(100,1)
   expect_length(x, 100)
 })
+
+
+test_that("rquad output the number within support", {
+  x <- rquad(100,1)
+  expect_all_true(x > -1 & x < 1)
+})
+
+
+
+
+
+test_that("rquad reproducibility under set.seed", {
+
+  set.seed(123)
+  x1 <- rquad(100,1)
+
+  set.seed(123)
+  x2 <- rquad(100,1)
+
+  expect_equal(x1, x2)
+})
