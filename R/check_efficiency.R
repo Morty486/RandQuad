ba2fc464abb1f1c230e2fdae545951c060bb13d8
Ayaka Ouyang <ayakaouyang@Ayakas-Laptop.local>
2026-09-09 10:20:42 -0500

    efficiency challenge

---
+++ R/check_efficiency.R
@@ -0,0 +1,26 @@
+
+#' Check the efficiency of the algorithm
+#'
+#' @param f function want to test
+#' @param n number of observations want to generate for a single function.
+#' @param theta function parameter
+#' @param B number of replicates
+#'
+#' @returns summary statistics of running time
+#' @export
+#'
+#' @examples
+#' check_efficiency(rquad)
+check_efficiency <- function(f,n=1000,theta=2,B=10){
+
+  Time <- replicate(B, system.time(f(n, theta))["elapsed"])
+
+  return(list=c(
+    Mean = mean(Time),
+    Median = median(Time),
+    SD = sd(Time),
+    Min = min(Time),
+    Max = max(Time)
+  ))
+
+}
